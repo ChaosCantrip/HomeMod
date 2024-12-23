e8c20d42755e67a3d72735e24701342de15243eb
ChaosCantrip <dev@chaoscantrip.com>
2024-12-23 01:45:06 +0000

    Added back on death

--- src/main/java/com/chaoscantrip/homemod/HomeMod.java
+++ src/main/java/com/chaoscantrip/homemod/HomeMod.java
@@ -1,6 +1,11 @@
 package com.chaoscantrip.homemod;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityManager;
@@ -8,6 +13,7 @@ import net.minecraftforge.common.capabilities.CapabilityToken;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.event.level.NoteBlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -22,6 +28,23 @@ public class HomeMod {
 
     public HomeMod() {
         MinecraftForge.EVENT_BUS.register(ModEventHandler.class);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SubscribeEvent
+    public void onLivingDeath(LivingDeathEvent event) {
+        System.out.println("LivingDeathEvent");
+        if (event.getEntity() instanceof Player player) {
+            System.out.println("Player died");
+            ServerLevel level = (ServerLevel) player.getServer().overworld();
+            HomeSavedData homeData = HomeSavedData.get(level);
+
+            ResourceKey<Level> dimension = player.level().dimension();
+            BlockPos pos = player.blockPosition();
+            Location deathLocation = new Location(pos, dimension.location().toString());
+
+            homeData.addBack(player.getUUID(), deathLocation);
+        }
     }
 }
 
